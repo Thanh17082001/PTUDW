04116d0425d6a63ec2da3e48f4e3072fde447ed6
Thanh17082001 <thienthanh17082001@gmail.com>
2023-02-03 23:29:05 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create =(req, res) =>{
+    res.send({Message: "create handler"});
+};
+
+exports.findALL =(req, res) =>{
+    res.send({Message: "findALL handler"});
+};
+
+exports.findOne =(req, res) =>{
+    res.send({Message: "findOne handler"});
+};
+
+exports.update =(req, res) =>{
+    res.send({Message: "update handler"});
+};
+
+exports.delete =(req, res) =>{
+    res.send({Message: "delete handler"});
+};
+
+exports.deleteAll =(req, res) =>{
+    res.send({Message: "deleteAll handler"});
+};
+
+exports.findALLFavorite =(req, res) =>{
+    res.send({Message: "findALLFavorite handler"});
+};
